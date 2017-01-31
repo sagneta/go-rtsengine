14656868147dd73be2e0eafbe68e7b4ce35e79dc
Stephen Agneta <you@example.com>
2017-01-31 14:31:56 -0500

    THinking about the A* Algorithm.

--- src/rtsengine/pathing.go
+++ src/rtsengine/pathing.go
@@ -4,3 +4,9 @@ package rtsengine
  Maintains the A* Pathing algorithm
 
 */
+
+// AStarPathing will implement the A* pathing algorithm
+// A simple description here: http://www.policyalmanac.org/games/aStarTutorial.htm
+// Psuedocode here at the bottom of this: http://web.mit.edu/eranki/www/tutorials/search/
+type AStarPathing struct {
+}
